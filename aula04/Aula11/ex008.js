37a4784a9beb85084fbc56ffc1a5a634df5deddb
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-05 17:42:27 -0300

    att

---
+++ aula04/Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`A velocidade do carro é ${vel}Km/h`)
+
+if(vel > 60) {   //condição simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO')
+}
+
+console.log('Dirija sempre com segurança')
