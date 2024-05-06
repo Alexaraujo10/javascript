c7abbd1abd3595ea4304eb38e8c8fd7e8d2f872d
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-06 17:45:11 -0300

    att

---
+++ aula04/Aula12ex/ex014/script.js
@@ -0,0 +1,24 @@
+
+function carregar(){
+var msg = document.getElementById('msg')
+var img = document.getElementById('manha')
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} Horas`
+if (hora >= 0 && hora < 12){
+    //Bom Dia
+    img.src  = 'manhã.png.jpg'
+    document.body.style.background = '#F2C89C'
+
+} else if (hora >= 12 && hora < 18){
+    //Boa Tarde
+    img.src = 'tarde.png.jpg'
+    document.body.style.background = '#FEB611'
+
+}else {
+    //Boa noite
+    img.src = 'noite.png.jpg'
+    document.body.style.background = '#07479C'
+}
+}
