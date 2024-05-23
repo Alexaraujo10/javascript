14910ce1f998c3e02e1eb5508a3e19b99588b2e8
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-23 17:16:21 -0300

    a

---
+++ aula04/aula13/index.js
@@ -0,0 +1,22 @@
+/*
+var c = 1
+while(c <= 7){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
+
+
+var c= 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
