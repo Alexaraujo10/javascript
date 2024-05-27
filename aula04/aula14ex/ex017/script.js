d653d2583e8259e7b3bd259d7defe1e4d006b1db
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-26 21:53:25 -0300

    a

--- aula04/aula14ex/ex017/script.js
+++ aula04/aula14ex/ex017/script.js
@@ -2,12 +2,14 @@ function tabuada(){
     let num = document.getElementById('txtn')
     let tab  = document.getElementById('seltab')
 
+    //Evita deixar o campo em branco
     if (num.value.length === 0){
         alert('Por favor digite um numero')
-      
+      //apartir do numero que colocar faz a tabuada do mesmo ate  10
     } else {
         let n = Number(num.value)
         let c = 1
+        //apos  fazer a tabuada do numero desejado e queira de um novo numero deixa o campo select em branco novamente
         tab.innerHTML = ''
         while (c <= 10){
             let item = document.createElement('option')
